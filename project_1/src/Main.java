3624c79cf18d5ea41223097b4c3d6078c256b912
zewazla <32648556+zewazla@users.noreply.github.com>
2019-02-19 20:46:29 +0000

    Add files via upload

--- project_1/src/Main.java
+++ project_1/src/Main.java
@@ -2,6 +2,29 @@ public class Main
 {
 	public static void main (String[] args)
 	{
-		System.out.println("nie wiem co mam wybrać");
+		//zadanie 2
+		System.out.println("Zadanie 2");
+		int wynik1, wynik2, wynik3, wynik4;
+		int l1 = 23;
+		int l2 = 65;
+		wynik1 = l1 + l2;
+		wynik2 = l1 - l2;
+		wynik3 = l1* l2;
+		wynik4 = l1/l2;
+		int wynik5 = l1%l2;
+		System.out.println("Wynik = " + wynik1);
+		System.out.println("Wynik = " + wynik2);
+		System.out.println("Wynik = " + wynik3);
+		System.out.println("Wynik = " + wynik4);
+		System.out.println("Wynik = " + wynik5);
+		wynik5++;
+		wynik5++;
+		wynik5++;
+		System.out.println("Wynik = " + wynik5);
+		wynik5--;
+		wynik5--;
+		wynik5--;
+		System.out.println("Wynik = " + wynik5);
+		System.out.println("");
 	}
 }
