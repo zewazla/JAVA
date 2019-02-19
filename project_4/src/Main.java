77af97df4c781105393f557d0bcf927c6e9a7ed6
zewazla <zewazla@gmail.com>
2019-02-19 21:37:49 +0000

    new file

---
+++ project_4/src/Main.java
@@ -0,0 +1,13 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        //zadanie 1
+        System.out.println("Zadanie 1");
+        byte b1 = 127;
+        b1++;
+        b1++;
+        System.out.println("Wynik b1 = " + b1);
+        System.out.println("");
+	}
+}
