effd69cc2723febaa591868a3d79ff68cf8c587d
zewazla <zewazla@gmail.com>
2019-02-20 09:48:29 +0000

    tablice jednowymiarowe

--- project_3/src/Main.java
+++ project_3/src/Main.java
@@ -1,9 +1,19 @@
+//tablice jedno i wielowymiarowe
+
 public class Main
 {
     public static void main (String[] args)
     {
-        int tab[] = new int[5];
+        int tab[] = new int[9];
         tab[0] = 6;
-        System.out.println(tab[0]);
+        tab[1] = 9;
+        tab[2] = 2;
+        tab[3] = 6;
+        tab[4] = 6;
+        for (int i = 0; i<tab.length; i++)
+        {
+            System.out.println("Element tablicy = " + tab[i]);
+        }
+
     }
 }
