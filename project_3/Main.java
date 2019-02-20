377753dd74b60ee25ff54fe17c52b72b9b210473
zewazla <zewazla@gmail.com>
2019-02-20 08:54:28 +0000

    new file

---
+++ project_3/Main.java
@@ -0,0 +1,13 @@
+// tablice jedno i wielowymiarowe
+
+public class Main
+{
+	public static void main (String[] args)
+	{
+		int tab[] = new int[5];
+		tab[0] = 6;
+		System.out.println(tab[0]);
+		
+	}
+	
+}
