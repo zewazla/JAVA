f8fbbcabc34f68b387d84701a786694f8b475a32
zewazla <zewazla@gmail.com>
2019-03-04 12:23:01 +0100

    homeworkUJ

---
+++ HomeWork_1/src/Table.java
@@ -0,0 +1,9 @@
+public class Table {
+    public static void main(String[] args){
+
+        for (int a = 1; a <=4; a++) {
+
+            System.out.printf("%3d %5d %5d\n", a, a * a, a * a * a );
+        }
+    }
+}
