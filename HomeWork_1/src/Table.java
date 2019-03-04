3e9145af3acbf4ed81407704064aa3ce507452ff
zewazla <zewazla@gmail.com>
2019-03-04 17:48:57 +0100

    chapter-2

--- HomeWork_1/src/Table.java
+++ HomeWork_1/src/Table.java
@@ -1,6 +1,9 @@
 public class Table {
     public static void main(String[] args){
 
+        System.out.printf("%3s %6s %5s\n", "b", "b^2", "b^3" );
+        System.out.printf("-------------------\n" );
+
         for (int a = 1; a <=4; a++) {
 
             System.out.printf("%3d %5d %5d\n", a, a * a, a * a * a );
