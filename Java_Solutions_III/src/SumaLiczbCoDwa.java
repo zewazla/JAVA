6b1fc5223474dbf3ab711c59e2ec04086e3407eb
zewazla <zewazla@gmail.com>
2019-03-06 22:22:40 +0100

    for_loop

---
+++ Java_Solutions_III/src/SumaLiczbCoDwa.java
@@ -0,0 +1,18 @@
+
+
+public class SumaLiczbCoDwa {
+    public static void main(String[] args) {
+        int suma = 0, i = 1;
+
+        while (i < 100) {
+            suma = suma + i;
+            i = i*3;
+        }
+
+
+            System.out.println(suma);
+
+
+
+    }
+}
---
+++ Java_Solutions_III/src/SumaLiczbCoDwa_for.java
@@ -0,0 +1,14 @@
+
+
+public class SumaLiczbCoDwa_for {
+    public static void main(String[] args) {
+        int suma = 0;
+
+        for (int i = 1; i < 10; i = 3*i) {
+            suma = suma + i;
+
+        }
+        System.out.println(suma);
+
+    }
+}
